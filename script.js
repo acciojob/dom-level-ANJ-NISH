c7803267fdea178a5fde67f0740bebe107ebd272
ANJ-NISH <155297183+ANJ-NISH@users.noreply.github.com>
2024-11-29 18:28:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let ele=document.getElementById("level");
+
+let level=2;
+
+while(ele.parentElement.tagName !== html)
+	{
+		level++;
+		ele=ele.parentElement;
+	}
+
+alert(`The level of the element is: ${level}`);
